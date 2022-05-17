0fc5e4dc70076271e4a829cab7044120b1780e91
thaisNY <71556315+thaisNY@users.noreply.github.com>
2022-05-17 12:36:36 -0300

    --

---
+++ BoxUnbox/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        int x = 20;
+        Object obj = x;
+        System.out.println(obj);
+        int y = (int) obj;
+        System.out.println(y);
+    }
+}
---
+++ BoxUnbox/src/Main2.java
@@ -0,0 +1,7 @@
+public class Main2 {
+    int x = 20;
+    Integer obj = x;
+    //System.out.println(obj);
+    int y = obj * 2;
+    //System.out.println(y);
+}
